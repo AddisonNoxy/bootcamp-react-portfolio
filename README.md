7adce7db1453e52a5cb94a2907fa92c2659304b5
AddisonNoxy <drolknil@gmail.com>
2023-08-15 17:37:08 -0400

    added contact form code

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Bootcamp React Portfolio
 
-
 ## Installation
-
 
 ## Usage
 
-
 ## Credits
 
+Route switching code in 'NavTabs.jsx' taken from the UNC Bootcamp example code
+
+Code for programming a React contact form with no backend taken from Ferenc Almasi's article
 
 ## License
+
 Please refer to the LICENSE in the repo.
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,10 +1,49 @@
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
 export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState('');
+
+    const submit = () => {
+        if (name && email && message) {
+
+            const serviceId = 'service_nbupdfl';
+            const templateId = 'template_kqbmo5g';
+            const userId = '9S11CsFtiBx_aYJtu';
+            const templateParams = {
+                name,
+                email,
+                message
+            };
+
+            emailjs.send(serviceId, templateId, templateParams, userId)
+                .then(response => console.log(response))
+                .then(error => console.log(error));
+
+            setName('');
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);        
+        } else {
+            alert('Please fill in all fields.');
+        }
+    }
+
+    const isValidEmail = email => {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(String(email).toLowerCase());
+    };
+
     return (
-        <div>
-            <h1>Contact</h1>
-            <p>
-                Filler text filler text
-            </p>
+        <div id="contact-form">
+            <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
+            <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
+            <textarea placeholder="Message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+            <button onClick={submit}>Send Message</button>
+            <span className={emailSent ? 'visible' : null}>Thank you for your message!</span>
         </div>
     )
 }
