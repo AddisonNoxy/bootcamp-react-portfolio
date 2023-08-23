e1c7d17efc1f0896109856758e6138428494d5f3
AddisonNoxy <drolknil@gmail.com>
2023-08-23 18:30:55 -0400

    fixed footer image pathing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
+import githubimg from '../../assets/icon-github-transparent.png';
+import linkedinimg from '../../assets/icon-linkedin-transparent.png';
+import overflowimg from '../../assets/icon-stackoverflow-transparent.png';
+
 export default function Footer() {
     return (
         <div className="grid grid-cols-3 justify-items-center pt-4">
-            <a href="https://github.com/AddisonNoxy"><img src="../../../assets/icon-github-transparent.png" className="w-16"></img></a>
-            <a href="https://linkedin.com"><img src="../../../assets/icon-linkedin-transparent.png" className="w-16"></img></a>
-            <a href="https://stackoverflow.com"><img src="../../../assets/icon-stackoverflow-transparent.png" className="w-16"></img></a>
+            <a href="https://github.com/AddisonNoxy"><img src={githubimg} className="w-16" /></a>
+            <a href="https://linkedin.com"><img src={linkedinimg} className="w-16" /></a>
+            <a href="https://stackoverflow.com"><img src={overflowimg} className="w-16" /></a>
         </div>
     )
 }
