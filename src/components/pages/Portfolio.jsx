3c164e60e0fd3e4c68b4febe573a520c6329bb37
AddisonNoxy <drolknil@gmail.com>
2023-08-23 18:26:04 -0400

    fixed image sources in portfolio

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,4 +1,9 @@
 import plannerimg from '../../../assets/bootcamp-planner.png';
+import codingimg from '../../../assets/coding-quiz.png';
+import databaseimg from '../../../assets/database-viewer.png';
+import forumimg from '../../../assets/movie-forum.png';
+import sportsimg from '../../../assets/sports-companion.png';
+import svgimg from '../../../assets/svg-maker.png';
 
 export default function Portfolio() {
     return (
@@ -12,27 +17,27 @@ export default function Portfolio() {
                     <p>A work day scheduler, created as part of the webdev bootcamp.</p>
                 </div>
                 <div className="card bg-cyan-600 w-96 text-center text-black pb-4">
-                    <figure><img src="../../assets/coding-quiz.png"></img></figure>
+                    <figure><img src={codingimg} /></figure>
                     <h1 className="text-xl font-bold underline hover:text-gray-600"><a href="https://github.com/AddisonNoxy/bootcamp-code-quiz">Javascript Coding Quiz</a></h1>
                     <p>An early assignment from the coding bootcamp, meant for a simple knowledge check on JavaScript.</p>
                 </div>
                 <div className="card bg-cyan-600 w-96 text-center text-black pb-4">
-                    <figure><img src="../../assets/database-viewer.png" className="max-h-48"></img></figure>
+                    <figure><img src={databaseimg} className="max-h-48" /></figure>
                     <h1 className="text-xl font-bold underline hover:text-gray-600"><a href="https://github.com/AddisonNoxy/database-viewer">Database Viewer</a></h1>
                     <p>A utility for connecting to and checking with the information in a database, with files for seeding an initial example database.</p>
                 </div>
                 <div className="card bg-cyan-600 w-96 text-center text-black pb-4">
-                    <figure><img src="../../assets/movie-forum.png"></img></figure>
+                    <figure><img src={forumimg} /></figure>
                     <h1 className="text-xl font-bold underline hover:text-gray-600"><a href="https://github.com/FelixW01/movie-forum-app">Movie Forum</a></h1>
                     <p>A collaborative project for viewing upcoming movies and allowing users to login and post comments.</p>
                 </div>
                 <div className="card bg-cyan-600 w-96 text-center text-black pb-4">
-                    <figure><img src="../../assets/svg-maker.png"></img></figure>
+                    <figure><img src={sportsimg} /></figure>
                     <h1 className="text-xl font-bold underline hover:text-gray-600"><a href="https://github.com/AddisonNoxy/svg-maker">SVG Maker</a></h1>
                     <p>A small utility for generating SVG images using the terminal.</p>
                 </div>
                 <div className="card bg-cyan-600 w-96 text-center text-black pb-4">
-                    <figure><img src="../../assets/sports-companion.png"></img></figure>
+                    <figure><img src={svgimg} /></figure>
                     <h1 className="text-xl font-bold underline hover:text-gray-600"><a href="https://github.com/Dinh282/sports-companion">Sports Companion</a></h1>
                     <p>A collaborative project for seeing information on different sports teams and upcoming games.</p>
                 </div>
